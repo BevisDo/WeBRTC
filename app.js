951f573506307848014d77e5da03f70aa16c6269
Anh Tuan <doanhtuan52@gmail.com>
2021-11-11 17:13:56 +0700

    Release WebRTC 1st

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars')
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const arrUserInfo =[]
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',function(req,res){
+    res.render('call')
+})
+
+io.on('connection', socket => {
+    socket.on('NGUOI_DUNG_DANG_KY',user=>{
+        const isExist = arrUserInfo.some(e => e.ten === user.ten)
+        socket.peerId = user.peerId;
+        if(isExist){
+            return socket.emit('DANG_KY_THAT_BAI');
+        }
+        arrUserInfo.push(user);
+        socket.emit('DANH_SACH_ONLINE',arrUserInfo);
+        socket.broadcast.emit('NEW',user);
+    })
+    socket.on('disconnect',()=>{
+        const index = arrUserInfo.findIndex(user => user.peerId===socket.peerId);
+        arrUserInfo.splice(index,1)
+        io.emit('NGAT_KET_NOI',socket.peerId);
+    })
+});
+  
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
+
+//custom 404 page
+app.use((req, res) => {
+    res.type('/text/plain');
+    res.status(404);
+    res.send('404 - not found');
+})
+
+//custom 500 page
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - server error');
+})
+// app.listen(port, () => console.log(
+//     `Dang chay o port ${port};` + 'nhan Ctrl - C de dung lai'
+// ))
---
+++ public/js/main.js
@@ -0,0 +1,76 @@
+const socket = io('http://localhost:3000');
+
+$('#chat').hide();
+
+socket.on('DANH_SACH_ONLINE',arrUserInfo=>{
+    $('#chat').show();
+    $('#sign').hide();
+    arrUserInfo.forEach(user=>{
+        const {ten,peerId} = user;
+        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
+    })
+    socket.on('NEW',user=>{
+        const {ten,peerId} = user;
+        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
+    })
+    socket.on('NGAT_KET_NOI',peerId=>{
+        $('#${peerId}').remove();
+    })
+})
+
+socket.on('DANG_KY_THAT_BAI',()=> alert('Dat lai!'))
+
+function openStream(){
+    const config = {Audio: false, video: true};
+    return navigator.mediaDevices.getUserMedia(config);
+}
+
+function playStream(idVideoTag,stream){
+    const video = document.getElementById(idVideoTag);
+    video.srcObject = stream;
+    video.play();
+}
+
+// openStream()
+// .then(stream => playStream('localStream',stream));
+
+//Tao id nguoi goi
+const peer = new Peer(); 
+peer.on('open',id => {
+    $('#my-id').append(id);
+    $('#btnSign').click(()=>{
+        const userName = $('#userName').val();
+        socket.emit('NGUOI_DUNG_DANG_KY',{ten: userName, peerId: id});
+    });
+});
+
+//nguoi goi
+$('#btnCall').click(()=>{
+    const id = $('#remote-id').val();
+    openStream()
+    .then(stream=>{
+        playStream('localStream',stream);
+        const call = peer.call(id,stream);
+        call.on('stream', remoteStream => playStream('remoteStream',remoteStream));       
+    })
+})
+
+//nguoi nhan
+peer.on ('call',call=>{
+    openStream()
+    .then(stream=>{
+        call.answer(stream);
+        playStream('localStream',stream);
+        call.on('stream',remoteStream =>playStream('remoteStream',remoteStream));
+    })
+})
+
+$('#ulUser').on('click','li',function(){
+    const id = $(this).attr('id');
+    openStream()
+    .then(stream=>{
+        playStream('localStream',stream);
+        const call = peer.call(id,stream);
+        call.on('stream', remoteStream => playStream('remoteStream',remoteStream));       
+    })
+})
